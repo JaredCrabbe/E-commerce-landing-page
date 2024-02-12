bf406d0e3f244e9c5e8794ff583bef29f4aebe38
Jared Crabbe <jdcrabbe1@gmail.com>
2024-02-12 10:31:12 +0200

    created image slider for catalogue
    Bug in carousel where when right or left is clicked twice then the opposite the first move is in the previous direction instead of the new one
    (left left right = left instead of right imediately)

--- src/index.js
+++ src/index.js
@@ -2,6 +2,62 @@ const initApp = () => {
   const hamburgerBtn = document.querySelector("#mobile-open-button");
   const mobileMenu = document.querySelector("#mobile-menu");
   const header = document.querySelector("#header");
+  const itemClassName = "carousel-card";
+  let items = document.getElementsByClassName(itemClassName);
+  let totalItems = items.length;
+  let activeSlide = 0;
+  let moving = false;
+
+  const nextBtn = document.getElementById("next");
+  const prevBtn = document.getElementById("prev");
+
+  function getNewPosition(direction) {
+    let totalWidth =
+      items[0].parentElement.parentElement.parentElement.clientWidth;
+    let singleAmountToMoveHorizontally = Math.floor(totalWidth / totalItems);
+    let singleItemWidth = items[0].clientWidth;
+
+    console.log(
+      `singleItemWidth = totalWidth:${totalWidth} / totalItems:${totalItems} = ${singleItemWidth}`
+    );
+
+    if (direction === "right") {
+      console.log("parameter -> move Right");
+      positionX = Math.floor(singleItemWidth * activeSlide);
+      console.log(
+        `positionX = singleItemWidth:${singleItemWidth} *
+        activeSlide:${activeSlide} = positionX:${positionX}`
+      );
+    } else if (direction === "left") {
+      console.log("parameter <- move left");
+      positionX = Math.floor(singleItemWidth * activeSlide + 1);
+      console.log(
+        `positionX = positionX - singleItemWidth:${singleItemWidth} *
+        activeSlide:${activeSlide} = positionX:${positionX}`
+      );
+    }
+    return positionX;
+  }
+
+  nextBtn.onclick = function() {
+    moveCards(-getNewPosition("right"));
+    activeSlide = (activeSlide + 1) % totalItems;
+    console.log(`activeSlide + 1 = ${activeSlide}`);
+  };
+  prevBtn.onclick = function() {
+    moveCards(-getNewPosition("left"));
+    activeSlide = (activeSlide - 1 + totalItems) % totalItems;
+    console.log(`activeSlide - 1 = ${activeSlide}`);
+  };
+
+  const moveCards = amount => {
+    for (let key in items) {
+      if (items.hasOwnProperty(key)) {
+        value = items[key];
+        items[key].style.transform = `translate(${amount}px)`;
+      }
+    }
+  };
 
   const toggleMenu = () => {
     mobileMenu.classList.toggle("hidden");
